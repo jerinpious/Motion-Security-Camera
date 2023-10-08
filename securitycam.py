161938187e88330234243c9465e11ab098d456a8
jerinpious <jpious4925@conestogac.on.ca>
2023-10-07 23:52:07 -0400

    alarm sound

--- securitycam.py
+++ securitycam.py
@@ -7,7 +7,15 @@ while True:
     _,img1=webcam.read()
     _,img2=webcam.read()
     diff=cv2.absdiff(img1,img2)
-    cv2.imshow("Security Camera",diff)
+    gray= cv2.cvtColor(diff,cv2.COLOR_BGR2GRAY)
+    _,thresh=cv2.threshold(gray,20,255,cv2.THRESH_BINARY)
+    contours,_=cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    for c in contours:
+        if cv2.contourArea(c)<5000:
+            continue
+        winsound.Beep(500,100)
+
+    cv2.imshow("Security Camera",thresh)
     if cv2.waitKey(10)==27:
         break
 webcam.release()
