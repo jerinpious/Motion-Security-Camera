a0711f47168ebdf6fb63b291f4877aa80aed27e0
jerinpious <jpious4925@conestogac.on.ca>
2023-10-07 23:20:44 -0400

    Output only motion cam

--- securitycam.py
+++ securitycam.py
@@ -6,7 +6,8 @@ webcam=cv2.VideoCapture(0)
 while True:
     _,img1=webcam.read()
     _,img2=webcam.read()
-    cv2.imshow("Security Camera",img1)
+    diff=cv2.absdiff(img1,img2)
+    cv2.imshow("Security Camera",diff)
     if cv2.waitKey(10)==27:
         break
 webcam.release()
